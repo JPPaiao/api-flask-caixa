cb1de8dc7e62ea6b06538e8d7c01a3066b8ba2e8
jplp <jp.paiao03@gmail.com>
2023-03-03 20:45:10 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+df = pd.read_excel('./planilhas/caixa-mensal-2023.xlsx', sheet_name=0)
+
+@app.route("/")
+def index():
+    return df.to_json()
+
+@app.route("/entradas")
+def entradas():
+    colunas_entrada = ["CAIXA", "PIX", "CARTAO", "IFOOD"]
+
+    def entrada_mes(colunas):
+        total = 0
+        contador = 0
+        total_entrada_mes = []
+
+        for col in colunas:
+            for contador in range(26):
+                total += df.at[contador, col]
+                contador += 1
+
+            total_entrada_mes.append(round(total, 2))
+            total = 0
+
+        return {
+            "total_entrada_mes": total_entrada_mes 
+        }
+    
+    response = entrada_mes(colunas_entrada)
+    
+    return response
+
+@app.route("/saidas")
+def saidas_mes():
+    colunas_saida = ["DIZ", "SALGADO", "EMBALAGENS", "AÇAI/MILK", "FEIRA/PAD.", "COMBUSTIVEL", "MERCADO", "PARTICULAR", "OUTROS"]
+
+    def saida_mes(colunas):
+        total = 0
+        contador = 0
+        total_saida_mes = []
+
+        for col in colunas:
+            for contador in range(26):
+                total += df.at[contador, col]
+                contador += 1
+
+            total_saida_mes.append(round(total, 2))
+            total = 0
+
+        return {
+            "total_saida_mes": total_saida_mes 
+        }
+
+    def saida_dia(colunas):
+        linhas = 1
+        dias = 1
+        total = 1
+        contador = 1
+        total_mensal = 0.00
+        total_diario = []
+
+        for contador in range(26):
+            for col in colunas:
+                if linhas > 25:
+                    break
+                
+                total += df.at[linhas, col]
+
+                if col == colunas[-1]:
+                    dia = {
+                        dias: round(total, 2)
+                    }
+
+                    total_diario.append(dia)
+                    total_mensal += total
+                    
+                    total = 0
+                    linhas += 1
+                    dias += 1
+
+        return {
+            "total_diario": total_diario,
+            "total_mensal": total_mensal
+        }
+    
+    response = [saida_dia(colunas_saida), saida_mes(colunas_saida)]
+
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
