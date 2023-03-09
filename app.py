4c0383bc584b8af834af234165a8f98b707a0f3e
JPPaião <jp.paiao03@gmail.com>
2023-03-09 18:27:32 -0300

    Colocando as datas das entradas e saidas do mês

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 import pandas as pd
+import json
 
 app = Flask(__name__)
+data = []
 df = pd.read_excel('./planilhas/caixa-mensal-2023.xlsx', sheet_name=0)
+for contador in range(25):
+    d = f"{df['DATA'][contador]}"
+    data.append(d)
 
 @app.route("/")
 def index():
-    return df
+    return df.to_json()
 
 @app.route("/entradas")
 def entradas():
@@ -14,27 +19,32 @@ def entradas():
 
     def entrada_mes(colunas):
         total = 0
-        contador = 0
-        total_entrada_mes = []
+        soma_entradas = []
+        lista_entradas = []
 
         for col in colunas:
             for contador in range(26):
                 total += df.at[contador, col]
                 contador += 1
 
-            total_entrada_mes.append(round(total, 2))
+            soma_entradas.append(total)
             total = 0
 
+        for contador in range(soma_entradas.__len__()):
+            lista_entradas.append({
+                colunas[contador]: soma_entradas[contador]
+            })
+
         return {
-            "total_entrada_mes": total_entrada_mes
+            "total_entrada_mes": lista_entradas
         }
 
-    def entrada_dia(colunas):
+    def entrada_dia(colunas, datas):
         linhas = 1
         dias = 1
-        total = 1
-        contador = 1
+        total = 0
         total_mensal = 0.00
+        entradas_dia = []
         total_diario = []
 
         for contador in range(26):
@@ -45,54 +55,61 @@ def entradas():
                 total += df.at[linhas, col]
 
                 if col == colunas[-1]:
-                    dia = {
-                        dias: round(total, 2)
-                    }
-
-                    total_diario.append(dia)
+                    entradas_dia.append(total)
                     total_mensal += total
 
                     total = 0
                     linhas += 1
                     dias += 1
 
+        for contador in range(len(datas)):
+            total_diario.append({
+                datas[contador]: entradas_dia[contador]
+            })
+
         return {
             "total_diario": total_diario,
             "total_mensal": total_mensal
         }
 
-    response = [entrada_mes(colunas_entrada), entrada_dia(colunas_entrada)]
+    response = [entrada_mes(colunas_entrada), entrada_dia(colunas_entrada, data)]
 
     return response
 
 @app.route("/saidas")
 def saidas():
-    colunas_saida = ["DIZ", "SALGADO", "EMBALAGENS", "AÇAI/MILK", "FEIRA/PAD.", "COMBUSTIVEL", "MERCADO", "PARTICULAR", "OUTROS"]
+    colunas_saida = ['DIZ', 'SALGADO', 'EMBALAGENS', 'AÇAI/MILK', 'FEIRA/PAD.', 'COMBUSTIVEL', 'MERCADO', 'PARTICULAR', 'OUTROS']
 
     def saida_mes(colunas):
         total = 0
-        contador = 0
-        total_saida_mes = []
+        soma_saidas = []
+        lista_saidas = []
 
         for col in colunas:
-            for contador in range(26):
-                total += df.at[contador, col]
+            for contador in range(25):
+                total += float(df.at[contador, col])
                 contador += 1
+                json.dumps(total)
 
-            total_saida_mes.append(float(round(total, 2)))
+            soma_saidas.append(total)
             total = 0
 
+        for contador in range(len(soma_saidas)):
+            lista_saidas.append({
+                colunas[contador]: soma_saidas[contador]
+            })
+
         return {
-            "total_saida_mes": total_saida_mes
+            "total_saidas": lista_saidas
         }
 
-    def saida_dia(colunas):
-        linhas = 1
+    def saida_dia(colunas, datas):
+        linhas = 0
         dias = 1
-        total = 1
-        contador = 1
+        total = 0
         total_mensal = 0.00
-        total_diario = []
+        saidas_dia = []
+        saidas_total_diaria = []
 
         for contador in range(26):
             for col in colunas:
@@ -102,23 +119,24 @@ def saidas():
                 total += df.at[linhas, col]
 
                 if col == colunas[-1]:
-                    dia = {
-                        dias: round(total, 2)
-                    }
-
-                    total_diario.append(dia)
+                    saidas_dia.append(total)
                     total_mensal += total
 
                     total = 0
                     linhas += 1
                     dias += 1
 
+        for contador in range(len(datas)):
+            saidas_total_diaria.append({
+                datas[contador]: saidas_dia[contador]
+            })
+
         return {
-            "total_diario": total_diario,
+            "total_diario": saidas_total_diaria,
             "total_mensal": total_mensal
         }
 
-    response = [saida_mes(colunas_saida), saida_dia(colunas_saida)]
+    response = [saida_mes(colunas_saida), saida_dia(colunas_saida, data)]
 
     return response
 
