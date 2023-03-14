b0caa6ec934c55091de9b702a19c736cbd5ed1b4
JPPaião <jp.paiao03@gmail.com>
2023-03-14 18:09:46 -0300

    Testando a rota de login

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from routers.auth.login import login_blueprint
 from routers.analise.analise import analise_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(login_blueprint, url_prefix='/login')
+app.register_blueprint(login_blueprint, url_prefix='/auth')
 app.register_blueprint(analise_blueprint, url_prefix='/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- routers/auth/login.py
+++ routers/auth/login.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 login_blueprint = Blueprint('login', __name__)
 
 @login_blueprint.route('/')
-def login():
+def auth():
     return 'login'
+
+@login_blueprint.route('/login', methods=['POST'])
+def login():
+    req = request.get_json()
+
+    nome = req['name']
+    idade = req['year']
+    cargo = req['cargo']
+
+    return [nome, idade, cargo]
+
