f0e93f31ad1803bda949536a5f5a60b82ee39459
JPPaião <jp.paiao03@gmail.com>
2023-03-16 19:26:59 -0300

    Integrando o banco de dados e adicionando usuario

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routers.auth.login import login_blueprint
 from routers.analise.analise import analise_blueprint
 
 app = Flask(__name__)
+
 app.register_blueprint(login_blueprint, url_prefix='/auth')
 app.register_blueprint(analise_blueprint, url_prefix='/')
 
--- routers/auth/login.py
+++ routers/auth/login.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+import MySQLdb
 
 login_blueprint = Blueprint('login', __name__)
 
@@ -10,9 +11,40 @@ def auth():
 def login():
     req = request.get_json()
 
-    nome = req['name']
-    idade = req['year']
-    cargo = req['cargo']
+    nome = req['nome']
+    senha = req['senha']
+    email = req['email']
 
-    return [nome, idade, cargo]
+    db = MySQLdb.connect(host='localhost', user='root', password='', database='db_python')
+    cursor = db.cursor()
 
+    cursor.execute('SELECT * FROM users')
+
+    res = cursor.fetchall()
+    res_list = list(res)
+
+    print(res_list)
+    db.commit()
+    cursor.close()
+
+    return { "res": res_list }
+
+@login_blueprint.route('/register', methods=['POST'])
+def register():
+    req = request.get_json()
+
+    name = req['nome']
+    senha = req['senha']
+    email = req['email']
+
+    db = MySQLdb.connect(host='localhost', user='root', password='', database='db_python')
+    cursor = db.cursor()
+
+    cursor.execute('INSERT INTO users VALUES (NULL, % s, % s, % s)', (name, senha, email, ))
+    db.commit()
+
+    print(db.fetchall())
+
+    cursor.close()
+
+    return 'ok'
