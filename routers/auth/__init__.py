d93dfa359e94f91b370c958ae73df3e7d4ba601f
JPPaião <jp.paiao03@gmail.com>
2023-03-18 23:58:53 -0300

    Adicionando rotas de update, delete e de usuarios

--- routers/auth/__init__.py
+++ routers/auth/__init__.py
@@ -11,7 +11,7 @@ def tratament_err(username=None, password=None, email=None, number=None):
                         "resposta": True,
                         "Error": f"{TypeError('Tipo errado')}"
                     }
-                elif a == password and type(a) != int or a == number and type(a) != int:
+                elif a == number and type(a) != int:
                     return {
                         "resposta": True,
                         "Error": f"{TypeError('Tipo errado')}"
@@ -36,6 +36,46 @@ def db_connect():
     db = MySQLdb.connect(host='localhost', user='root', password='', database='db_python')
     return db
 
+def users_activate():
+    db = db_connect()
+    cursor = db.cursor()
+
+    try:
+        cursor.execute(f'''
+            SELECT name, email, number FROM users;
+        ''')
+        users = cursor.fetchall()
+        db.commit()
+
+        return {
+            "Users": users
+        }
+    except Exception as err:
+        return { "Error": f"Error: {err}" }
+    finally:
+        cursor.close()
+        db.close()
+
+def user(id):
+    db = db_connect()
+    cursor = db.cursor()
+
+    try:
+        cursor.execute(f'''
+            SELECT name, email, number FROM users WHERE id='{id}';
+        ''')
+        user_active = cursor.fetchall()
+        db.commit()
+
+        return {
+            "User": user_active,
+        }
+    except Exception as err:
+        return { "Error": f"Error: {err}" }
+    finally:
+        cursor.close()
+        db.close()
+
 def resister_user(username, password, email, number):
     db = db_connect()
     cursor = db.cursor()
@@ -44,17 +84,19 @@ def resister_user(username, password, email, number):
         validate = tratament_err(username=username, password=password, email=email, number=number)
 
         if validate["resposta"]:
-            return { "Eroor": validate["Error"] }
+            return { "Error": validate["Error"] }
         else:
             cursor.execute('INSERT INTO users VALUES (NULL, %s, %s, %s, %s)', (username, password, email, number))
             db.commit()
-            cursor.close()
 
             return {
-                'Sucess': f'Usuario {username} adicionado com sucesso!'
+                'Sucess': f'Usuário {username} adicionado com sucesso!'
             }
     except ValueError as err:
         return { 'Error': f'ValueError: {err}!' }
+    finally:
+        cursor.close()
+        db.close()
 
 def login_user(password, email):
     db = db_connect()
@@ -67,13 +109,13 @@ def login_user(password, email):
             return { "Error": validate['Error'] }
         else:
             cursor.execute(f'''
-                SELECT id, name FROM users WHERE password = '{password}' AND email = '{email}';
+                SELECT * FROM users WHERE password = '{password}' AND email = '{email}';
             ''')
             user_encontrado = cursor.fetchall()
             db.commit()
-            cursor.close()
+
             if user_encontrado == ():
-                return { "Error": "E-mail ou senha invalidos!!" }
+                return { "Error": "E-mail ou senha inválidos!!" }
             else:
                 return {
                     "User": user_encontrado,
@@ -81,4 +123,56 @@ def login_user(password, email):
                 }
     except ValueError as err:
         return { 'Error': f'ValueError: {err}!' }
+    finally:
+        cursor.close()
+        db.close()
 
+def update_user(username, password, email, number, id):
+    db = db_connect()
+    cursor = db.cursor()
+
+    try:
+        validate = tratament_err(username=username, password=password, email=email, number=number)
+
+        if validate['resposta']:
+            return { "Error": validate['Error'] }
+        else:
+            cursor.execute(f'''
+                UPDATE users SET name='{username}', password='{password}', email='{email}', number='{number}' WHERE id='{id}'
+            ''')
+
+            db.commit()
+
+            return {
+                "Sucess": "Alterações feitas com sucesso"
+            }
+    except Exception as err:
+        return { 'Error': f'Error: {err}'}
+    finally:
+        cursor.close()
+        db.close()
+
+def delete_user(id):
+    db = db_connect()
+    cursor = db.cursor()
+
+    try:
+        cursor.execute(f''' SELECT name, id FROM users WHERE id='{id}' ''')
+        db.commit()
+
+        if cursor.fetchall() == ():
+            cursor.execute(f'''
+                DELETE FROM users WHERE id='{id}'
+            ''')
+            db.commit()
+
+            return {
+                "Sucess": "Usuario deletado com sucesso"
+            }
+        else:
+            return { "Error": "Usuario não encontrado" }
+    except Exception as err:
+        return { 'Error': f'Error: {err}' }
+    finally:
+        cursor.close()
+        db.close()
--- routers/auth/login.py
+++ routers/auth/login.py
@@ -1,11 +1,25 @@
 from flask import Blueprint, request
-from . import resister_user, login_user
+from . import resister_user, login_user, update_user, delete_user, user, users_activate
 
 login_blueprint = Blueprint('login', __name__)
 
-@login_blueprint.route('/')
-def auth():
-    return 'login'
+@login_blueprint.route('/users')
+def users():
+    try:
+        response = users_activate()
+    except Exception as err:
+        response =  { "Error": f"Error: {err}"}
+
+    return response
+
+@login_blueprint.route('/<int:id>')
+def auth(id):
+    try:
+        response = user(id)
+    except Exception as err:
+        response =  { "Error": f"Error: {err}"}
+
+    return response
 
 @login_blueprint.route('/login', methods=['POST'])
 def login():
@@ -17,7 +31,7 @@ def login():
 
         respose = login_user(password, email)
     except KeyError as err:
-        return { "Error": f"KeyError: {err}" }
+        respose = { "Error": f"KeyError: {err}" }
 
     return respose
 
@@ -34,14 +48,41 @@ def register():
 
         response = resister_user(name, password, email, number)
     except KeyError as err:
-        return { "Error": f"KeyError: {err}" }
+        response = { "Error": f"KeyError: {err}" }
 
     return response
 
-@login_blueprint.route('/edit', methods=['PUT'])
-def edit():
-    req = request.get_json()
+@login_blueprint.route('/update/<int:id>', methods=['PUT'])
+def update(id):
+    if request.method == 'PUT':
+        req = request.get_json()
 
-    
+        if req.get('name'):
+            name = req['name']
+        if req.get('password'):
+            password = req['password']
+        if req.get('email'):
+            email = req['email']
+        if req.get('number'):
+            number = req['number']
+
+        if name or password or email or number:
+            response = update_user(username=name, password=password, email=email, number=number, id=id)
+
+    return response
+
+@login_blueprint.route('/delete/<int:id>', methods=['DELETE'])
+def delete(id):
+    if request.method == 'DELETE':
+        req = request.get_json()
+        password = req['password_delete']
+
+        if password == 3003:
+            response = delete_user(id)
+        else:
+            response = { 'Error': 'Senha inválida' }
+
+    return response
+
 
 
