e32dc70a8667dc0daeaa72e4d4aede9c3f2a3550
jplp <jp.paiao03@gmail.com>
2023-07-14 17:02:10 -0300

    Adicionado funções de crud de usuários

--- routers/auth/connection_db.py
+++ routers/auth/connection_db.py
@@ -1,3 +1,6 @@
-from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy import create_engine
 
 engine = create_engine('mysql+mysqlconnector://root@localhost:3306/db_python', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
--- routers/auth/login.py
+++ routers/auth/login.py
@@ -1,21 +1,145 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
-from connection_db import engine
+from tables_class import User
+from connection_db import session
 
-Base = declarative_base()
+def login_user(data_user):
+    try:
+        user = session.query(User).filter(User.email == data_user.email).one()
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(55))
-    password = Column(String(55))
-    email = Column(String(55))
-    number = Column(Integer)
+        if user:
+            if user.password == data_user.password:
+                return {
+                    "user": user
+                }
+            else:
+                return {
+                    "Error": "Email ou senha não inválido"
+                }
+        else:
+            return {
+                "Error": "Erro usuário não encontrado"
+            }
+    except Exception as e:
+        session.rollback()
 
-user = User(username='Lucas', password='1234', email='test@gmail.com', number=1234567)
-Session = sessionmaker(bind=engine)
-session = Session()
+        return {
+            "Error": "Erro ao fazer login"
+        } 
+    finally:
+        session.close()
 
-session.add(user)
-session.commit()
+# ADICIONANDO
+def set_user(new_user):
+    if new_user:
+        try:
+            user = User(username=new_user.username, password=new_user.password, email=new_user.email, number=new_user.number)
+            session.add(user)
+            session.commit()
 
+            return {
+                "Success": "Usuario adicionado com sucesso"
+            }
+        except Exception as e:
+            session.rollback()
+
+            return {
+                "Error": "Erro usuário não pode ser adicionado"
+            } 
+        finally:
+            session.close()       
+    else:
+        return {
+                "Error": "Erro usuário não pode ser adicionado"
+            }
+
+# BUSCANDO
+def get_user(email):
+    try:
+        user = session.query(User).filter(User.email == email)
+
+        if user:
+            return {
+                "user": user
+            }
+        else:
+            return {
+                "Error": "Usuário não encontrado"
+            }
+    except Exception as e:
+        session.rollback()
+
+        return {
+            "Error": "Erro ao encontrar usuário"
+        } 
+    finally:
+        session.close()   
+
+
+def get_users_all():
+    users_dict = []
+
+    try:
+        users = session.query(User).all()
+
+        for user in users:
+            user_dict = {
+                'id': user.id,
+                'username': user.username,
+                'password': user.password,
+                'email': user.email,
+                'number': user.number
+            }
+
+            users_dict.append(user_dict)
+    except Exception as e:
+        session.rollback()
+
+        return {
+            "Error": "Erro ao executar essa operação"
+        }
+    finally:
+        session.close()
+    
+    return users_dict
+
+
+# MODIFICANDO
+def update_user(id, update):
+    try:
+        user = session.query(User).filter(User.id == id)
+
+        if user:
+            user = user.update(update, synchronize_session='fetch')
+            session.commit()
+        else:
+            return {
+                "Error": "Usuário não encontrado"
+            }
+    except Exception as e:
+        session.rollback()
+
+        return {
+            "Error": "Erro ao tentar atulizar usuário"
+        }
+    finally:
+        session.close()
+
+# DELETANDO
+def delete_user(id):
+    try:
+        user = session.query(User).get(id)
+
+        if user:
+            session.delete(user)
+            session.commit()
+        else:
+            return {
+                "Error": "Usuário não encontrado"
+            }
+    except Exception as e:
+        session.rollback()
+
+        return {
+            "Error": "Erro ao excluir usuário"
+        }
+    finally:
+        session.close()
