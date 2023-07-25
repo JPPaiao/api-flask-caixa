1edd31676fd213bfca1f72823ea794802cb7dca7
jplp <jp.paiao03@gmail.com>
2023-07-25 19:00:20 -0300

    Adicionado os totais dos dias na função set_month

--- routers/analise/table_month.py
+++ routers/analise/table_month.py
@@ -1,7 +1,30 @@
 def order_list_month(data):
     return data['date']
 
+def set_total_day(list_day):
+    total = 0
+
+    for day in list_day:
+        if day == 'days':
+            for row in list_day[day]:
+                if row['description'] == 'inputs':
+                    total += row['value']
+                else: 
+                    total -= row['value']
+
+    return total
+
+def create_entry(id, value, date, description, text):
+    return {
+        'id': id,
+        'value': value,
+        'date': date,
+        'description': description,
+        'text': text
+    }
+
 def set_month(months):
+    data_months = {}
     all_months = [
         'janeiro',
         'fevereiro',
@@ -16,16 +39,6 @@ def set_month(months):
         'novembro',
         'dezembro'
     ]
-    data_months = {}
-
-    def create_entry(id, value, date, description, text):
-        return {
-            'id': id,
-            'value': value,
-            'date': date,
-            'description': description,
-            'text': text
-        }
     
     for key, value in months.items():
         if key == 'date':
@@ -35,15 +48,23 @@ def set_month(months):
         month_name = all_months[int(month)]
 
         for column in value['columns']:
-            entry = create_entry(id=1, value=column, date=value['date'], description=key, text=value[column])
-            
+            entry = create_entry(id=1, value=value[column], date=value['date'], description=key, text=column)
+
             if month_name not in data_months:
                 data_months[month_name] = {}
 
             if int(day) not in data_months[month_name]:
-                data_months[month_name][int(day)] = []
+                data_months[month_name][int(day)] = {
+                    'total_day': 0,
+                    'days': []
+                }
 
-            data_months[month_name][int(day)].append(entry)
+            data_months[month_name][int(day)]['days'].append(entry)
+    
+    for month in data_months:
+        for day in data_months[month]:
+            total_day = set_total_day(data_months[month][day])
+            data_months[month][day]['total_day'] = total_day
 
     return data_months
 
